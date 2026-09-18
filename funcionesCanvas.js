8f77cb29a2b60dda682ae4fc14fbbf9c0835a185
ZahidN09 <zahid09nazateej@gmail.com>
2026-09-18 14:46:02 -0500

    Deteccion de colision y generacion aleatoria de comida

--- funcionesCanvas.js
+++ funcionesCanvas.js
@@ -4,8 +4,8 @@ let ctx = canvas.getContext("2d");
 const COLOR_SNAKE = "#2cac12";
 const COLOR_COMIDA = "#b30000";
 const LADO_CUADRADO = 25;
-const POS_INIT_X = Math.floor((canvas.width - LADO_CUADRADO) / 2);
-const POS_INIT_Y = Math.floor((canvas.height - LADO_CUADRADO) / 2);
+const POS_INIT_X = 225;//Math.floor((canvas.width - LADO_CUADRADO) / 2);
+const POS_INIT_Y = 225;//Math.floor((canvas.height - LADO_CUADRADO) / 2);
 const DESPLAZAMIENTO = 25;
 const POS_COMIDA_X = POS_INIT_X + 150;
 const POS_COMIDA_Y = POS_INIT_Y;
@@ -13,6 +13,9 @@ const POS_COMIDA_Y = POS_INIT_Y;
 let direccion = "R";
 let direccion_new = "R";
 let direccion_old = "R";
+
+let comidaX = POS_COMIDA_X;
+let comidaY = POS_COMIDA_Y;
 
 function limpiarCanva() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -50,8 +53,9 @@ function moverSnake() {
 
     snake.unshift([snake[0][0] + cambio_x * DESPLAZAMIENTO, snake[0][1] + cambio_y * DESPLAZAMIENTO]);
     snake.pop();
-    dibujarComida();
     dibujarSnake();
+    detectarColision();
+    dibujarComida();
 }
 
 function moverDerecha() {
@@ -77,20 +81,31 @@ function moverAbajo() {
 function validarCambioDireccion() {
     if ((direccion_new == "R") && (direccion_old == "L")) {
         direccion = "L";
-    }else if((direccion_new == "L") && (direccion_old == "R")){
+    } else if ((direccion_new == "L") && (direccion_old == "R")) {
         direccion = "R";
-    }else if((direccion_new == "U") && (direccion_old == "D")){
+    } else if ((direccion_new == "U") && (direccion_old == "D")) {
         direccion = "D";
-    }else if((direccion_new == "D") && (direccion_old == "U")){
+    } else if ((direccion_new == "D") && (direccion_old == "U")) {
         direccion = "U";
-    }else{
+    } else {
         direccion = direccion_new;
         direccion_old = direccion_new;
     }
 }
 
-function dibujarComida(){
-    let comidaX = POS_COMIDA_X;
-    let comidaY = POS_COMIDA_Y;
+function dibujarComida() {
     dibujarRectangulo(comidaX, comidaY, LADO_CUADRADO, LADO_CUADRADO, COLOR_COMIDA);
+}
+
+function detectarColision() {
+    if (snake[0][0] == comidaX && snake[0][1] == comidaY) {
+        comidaX = LADO_CUADRADO * generarAleatorio(0, canvas.width / LADO_CUADRADO);
+        comidaY = LADO_CUADRADO * generarAleatorio(0, canvas.height / LADO_CUADRADO);
+    }
+}
+
+function generarAleatorio(min, max) {
+    let random = Math.random();
+    let numero = Math.ceil(random * (max - min)) + min;
+    return numero - 1;
 }
