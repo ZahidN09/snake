aae471416206ad45f30b1e46c4ea4026f4c62f2b
ZahidN09 <zahid09nazateej@gmail.com>
2026-09-17 22:17:24 -0500

    Mejora en movimiento

--- funcionesCanvas.js
+++ funcionesCanvas.js
@@ -5,13 +5,11 @@ const COLOR_SNAKE = "#2cac12";
 const LADO_CUADRADO = 25;
 const POS_INIT_X = Math.floor((canvas.width - LADO_CUADRADO) / 2);
 const POS_INIT_Y = Math.floor((canvas.height - LADO_CUADRADO) / 2);
-const DESPLAZAMIENTO = 10;
-
-let largoSnake = 3;
-let snakeX = 0;
-let snakeY = 0;
+const DESPLAZAMIENTO = 25;
 
 let direccion = "R";
+let direccion_new = "R";
+let direccion_old = "R";
 
 function limpiarCanva() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -23,40 +21,66 @@ function dibujarRectangulo(X, Y, ancho, alto, color) {
 }
 
 function dibujarSnake() {
-    let x = snakeX;
-    let y = snakeY;
-    for (let i = 1; i <= largoSnake; i++) {
-        dibujarRectangulo(x, y, LADO_CUADRADO, LADO_CUADRADO, COLOR_SNAKE);
-        x = x - LADO_CUADRADO;
+    for (let i = 1; i <= snake.length; i++) {
+        dibujarRectangulo(snake[i - 1][0], snake[i - 1][1], LADO_CUADRADO, LADO_CUADRADO, COLOR_SNAKE);
     }
 }
 
 function moverSnake() {
-    if (direccion == "R") {
-        snakeX = snakeX + DESPLAZAMIENTO;
-    } else if (direccion == "L") {
-        snakeX = snakeX - DESPLAZAMIENTO;
-    } else if (direccion == "U") {
-        snakeY = snakeY - DESPLAZAMIENTO;
-    } else if (direccion == "D") {
-        snakeY = snakeY + DESPLAZAMIENTO;
-    }
+    let cambio_x = 0;
+    let cambio_y = 0;
+
     limpiarCanva();
+    if (direccion == "R") {
+        cambio_x = 1;
+        cambio_y = 0;
+    } else if (direccion == "L") {
+        cambio_x = -1;
+        cambio_y = 0;
+    } else if (direccion == "U") {
+        cambio_x = 0;
+        cambio_y = -1;
+    } else if (direccion == "D") {
+        cambio_x = 0;
+        cambio_y = 1;
+    }
+
+    snake.unshift([snake[0][0] + cambio_x * DESPLAZAMIENTO, snake[0][1] + cambio_y * DESPLAZAMIENTO]);
+    snake.pop();
     dibujarSnake();
 }
 
 function moverDerecha() {
-    direccion = "R";
+    direccion_new = "R";
+    validarCambioDireccion();
 }
 
 function moverIzquierda() {
-    direccion = "L";
+    direccion_new = "L";
+    validarCambioDireccion();
 }
 
 function moverArriba() {
-    direccion = "U";
+    direccion_new = "U";
+    validarCambioDireccion();
 }
 
 function moverAbajo() {
-    direccion = "D";
+    direccion_new = "D";
+    validarCambioDireccion();
+}
+
+function validarCambioDireccion() {
+    if ((direccion_new == "R") && (direccion_old == "L")) {
+        direccion = "L";
+    }else if((direccion_new == "L") && (direccion_old == "R")){
+        direccion = "R";
+    }else if((direccion_new == "U") && (direccion_old == "D")){
+        direccion = "D";
+    }else if((direccion_new == "D") && (direccion_old == "U")){
+        direccion = "U";
+    }else{
+        direccion = direccion_new;
+        direccion_old = direccion_new;
+    }
 }
--- snake.js
+++ snake.js
@@ -1,11 +1,19 @@
-let velocidad = 200;
+let largoSnake = 3;
+let snake = [];
+let snakeX = 0;
+let snakeY = 0;
 
-function iniciar(){
+let velocidad = 500;
+
+function iniciar() {
     snakeX = POS_INIT_X;
     snakeY = POS_INIT_Y;
+    for (let i = 0; i <= largoSnake - 1; i++) {
+        snake[i] = [snakeX - (i * LADO_CUADRADO), snakeY];
+    }
     dibujarSnake();
 }
 
-function start(){
+function start() {
     intervalo = setInterval(moverSnake, velocidad);
 }
