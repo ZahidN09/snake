1e5192f2309225d85c1003c37c151d12e555484b
ZahidN09 <zahid09nazateej@gmail.com>
2026-09-18 17:15:56 -0500

    Botón de inicio permite empezar de nuevo

--- snake.js
+++ snake.js
@@ -4,10 +4,18 @@ let snakeX = 0;
 let snakeY = 0;
 
 let velocidadActual = 500;
+let flag = 0;
 
 function iniciar() {
+    puntaje = 0
+    mostarEnSpan("txtPuntaje", puntaje);
+    largoSnake = 3;
+    snake = [];
+    velocidadActual = 500;
     snakeX = POS_INIT_X;
     snakeY = POS_INIT_Y;
+    comidaX = POS_COMIDA_X;
+    comidaY = POS_COMIDA_Y;
     for (let i = 0; i <= largoSnake - 1; i++) {
         snake[i] = [snakeX - (i * LADO_CUADRADO), snakeY];
     }
@@ -16,5 +24,12 @@ function iniciar() {
 }
 
 function start() {
+    flag = flag + 1;
+    if(flag > 1){
+        clearInterval(intervalo);
+        flag = 1;
+    }
+    limpiarCanva();
+    iniciar();
     intervalo = setInterval(moverSnake, velocidadActual);
 }
